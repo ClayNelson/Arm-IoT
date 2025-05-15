847b8047877e3ad0b3299b9ca51fa4c96b06f644
Clay Nelson <claynelson@github.com>
2025-05-15 16:30:31 -0400

    Fix floating-point ABI settings for ARM cross-compilation

--- toolchains/arm-toolchain.cmake
+++ toolchains/arm-toolchain.cmake
@@ -1,20 +1,19 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Specify the cross compiler
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+# Specify the cross compiler with full paths
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
 
-# Set the target system root
+# Set the target system root and search paths
 set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
-
-# Search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
-# Search for libraries and headers in the target directories
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Flags for Raspberry Pi
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a -mfloat-abi=softfp -mfpu=neon-fp-armv8")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a -mfloat-abi=softfp -mfpu=neon-fp-armv8")
+# Set ARM architecture and FPU flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
+
+# Set linker flags to ensure consistent FPU usage
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
