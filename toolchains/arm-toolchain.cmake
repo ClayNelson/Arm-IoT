53f53650e3b72482b9f621e23991792a3f61f579
Clay Nelson <claynelson@github.com>
2025-05-16 15:48:24 -0400

    Update ARM toolchain and improve build process

--- toolchains/arm-toolchain.cmake
+++ toolchains/arm-toolchain.cmake
@@ -1,9 +1,10 @@
+# Set system name and processor
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Specify the cross compiler with full paths
-set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+# Specify the cross compiler
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
 # Set the target system root and search paths
 set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
@@ -11,9 +12,17 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Set ARM architecture and FPU flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
+# Set ARM architecture and FPU flags for Raspberry Pi
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -mcpu=cortex-a7")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -mcpu=cortex-a7")
 
-# Set linker flags to ensure consistent FPU usage
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8")
+# Set linker flags
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -mcpu=cortex-a7")
+
+# Enable position independent code
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+# Set debug flags
+set(CMAKE_BUILD_TYPE Debug)
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g3 -O0")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g3 -O0")
